bb9b8256405cf362b1f86646eb729dfbee93d5c9
JSU <nonexistent@jsu.edu>
2020-01-28 16:45:11 -0600

    Changed the class to reflect the example we were given

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -14,55 +14,22 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    final Button buttonRock = (Button) findViewById(R.id.buttonRock);
-    final Button buttonPaper = (Button) findViewById(R.id.buttonPaper);
-    final Button buttonScissors = (Button) findViewById(R.id.buttonScissors);
-    int user;
-    int computer;
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Weapon player;
+    Weapon computer;
+    TextView winner = (TextView) findViewById(R.id.winText);
+    TextView playerWeapon = (TextView) findViewById(R.id.playerWeapon);
+    TextView computerWeapon = (TextView) findViewById(R.id.computerWeapon);
+    TextView score = (TextView) findViewById(R.id.scoreText);
+    private Button rButton;
 
+    private Weapon randomWeapon() {
+        int pick = new Random().nextInt(Weapon.values().length);
+        return Weapon.values()[pick];
+    }
 
-
-
-
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            TextView t = (TextView) findViewById(R.id.playerWeapon);
-            int opponet;
-            int choice;
-
-            if (v.equals(buttonRock)) {
-
-                t.setText("Player's Weapon: Rock");
-                user = 1;
-                choice = (int) (Math.random() * (3 - 1) + 1) + 1;
-                opponet = opponet.computerChoice(choice);
-                computeWin(opponet, 1);
-
-
-                if (v.equals(buttonPaper)) {
-                    t.setText("Player's Weapon: Paper");
-                    user = 2;
-                    choice = (int) (Math.random() * (3 - 1) + 1) + 1;
-                    opponet = opponet.computerChoice(choice);
-                    computeWin(opponet, 2);
-                }
-                if (v.equals(buttonScissors)) {
-                    t.setText("Player's Weapon: Scissor");
-                    user = 3;
-                    choice = (int) (Math.random() * (3 - 1) + 1) + 1;
-                    opponet = opponet.computerChoice(choice);
-                    computeWin(opponet, 3);
-                } else {
-                    user = 0;
-                }
-            }
-        }
-
-        ;
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,19 +37,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        score.setText("Player: , Computer: ");
+        playerWeapon.setText("Player Weapon: ");
+        computerWeapon.setText("Computer Weapon: ");
+        winner.setText("");
+        rButton = findViewById(R.id.buttonRock);
+        rButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonRock:
+
+                player = Weapon.ROCK;
+                playerWeapon.setText("Player Weapon:" + player);
+                computer = randomWeapon();
+                computerWeapon.setText("Computer Weapon: " + computer);
+                winner.setText(whoWins());
+                score.setText("Player: "+ playerScore() +" , Computer: "+ computerScore());
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -105,4 +81,105 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public enum Weapon {
+
+        ROCK("Rock"),
+        PAPER("Paper"),
+        SCISSORS("Scissors");
+        private String message;
+
+        private Weapon(String msg) {
+            message = msg;
+        }
+
+        @Override
+        public String toString() {
+            return message;
+        }
+
+    }
+
+    public int isWin() {
+        int win;
+        if ( player = Weapon.ROCK; && computer = Weapon.SCISSORS;){
+
+            win =1;
+                    //"Player wins ...  Rock crushes Scissors!";
+
+        }
+        if ( player.equals(Weapon.PAPER); && computer.equals(Weapon.ROCK);){
+
+            win =2;
+        //"Player wins ... Paper covers Rock!";
+
+        }
+        if(player = Weapon.SCISSORS; && computer=Weapon.PAPER;){
+            win=5;
+        }
+        if (player == computer) {
+            win = 3;
+            //"Its a Draw!";
+
+        }
+
+
+        else {
+
+            win =4;
+            //"Computer player Wins!";
+
+        }
+
+        return win;
+
+
+    }
+    public String whoWins(){
+        String whoWins = null;
+
+        switch(isWin()){
+            case 1:
+                whoWins="Player wins ...  Rock crushes Scissors!";
+                break;
+            case 2:
+                whoWins="Player wins ... Paper covers Rock!";
+                break;
+            case 3:
+                whoWins="It is a Draw!";
+                break;
+            case 4:
+                whoWins="Computer player Wins!";
+                break;
+            case 5:
+                whoWins=" Player wins.... Scissors cuts Paper"
+        }
+
+        return whoWins;
+
+    }
+    public int playerScore(){
+        isWin();
+        int pScore =0;
+
+        if(isWin() == 1){
+            ++pScore;
+        }
+        if(isWin() == 2){
+            ++pScore;
+        }
+        return pScore;
+    }
+    public int computerScore(){
+        isWin();
+        int cScore =0;
+
+        if(isWin() == 4) {
+            ++cScore;
+        }
+        return cScore;
+    }
 }
+
+
+
